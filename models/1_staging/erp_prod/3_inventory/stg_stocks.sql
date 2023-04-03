99ef1fec0c8eeb3fb5e9317f9a87debe8087ceb0
anmar@floranow.com <anmar@floranow.com>
2023-04-03 19:27:43 +0000

    u

--- models/1_staging/erp_prod/3_inventory/stg_stocks.sql
+++ models/1_staging/erp_prod/3_inventory/stg_stocks.sql
@@ -3,7 +3,18 @@ With source as (
 )
 select 
 
-*,
+id as stock_id,
+name as stock_name,
+warehouse_id,
+reseller_id,
+
+
+case when stock_type = 0 then 'inventory' else 'flying' end as stock_type,
+case when status = 0 then 'visible' else 'hidden' end as stock_status,
+
+out_feed_source_id,
+
+availability_type,
 
 current_timestamp() as ingestion_timestamp,
  
@@ -11,4 +22,4 @@ current_timestamp() as ingestion_timestamp,
 
 
 
-from source as stk
+from source as st
