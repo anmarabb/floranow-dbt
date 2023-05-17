3a5ff13d199851d8a0ceb81f1fd2f4692677378c
Anmar Abbas <anmar@floranow.com>
2023-05-17 09:29:54 +0000

    y

--- models/1_staging/erp_prod/2_shipments/int_shipments.sql
+++ models/1_staging/erp_prod/2_shipments/int_shipments.sql
@@ -3,7 +3,7 @@ With source as (
     
  select 
  
- sh.* EXCEPT(ingestion_timestamp,master_shipment_id),
+sh.* EXCEPT(ingestion_timestamp,master_shipment_id,departure_date),
 
 
 msh.master_shipments_status,
@@ -11,8 +11,13 @@ msh.master_shipment_name as master_shipment,
 msh.master_shipments_fulfillment_status,
 msh.arrival_at,
 msh.master_shipment_id,
+msh.master_total_quantity,
+msh.departure_date,
+msh.arrival_date,
 
-concat( "https://erp.floranow.com/master_shipment/", msh.master_shipment_id) as master_shipment_link,
+
+
+concat( "https://erp.floranow.com/master_shipments/", msh.master_shipment_id) as master_shipment_link,
 
 shipments_suppliers.supplier_name as Supplier,
 shipments_suppliers.supplier_region as Origin,
@@ -27,15 +32,16 @@ case when msh.arrival_at is not null then 1 else 0 end as shipments_received,
 case when msh.arrival_at is null  then 'shipmnet_not_arrived' else 'shipmnet_arrived' end as shipmnet_arrival,
 
 
+
 case 
-    when date_diff(date(sh.arrival_date)  ,current_date(), month) > 1 then 'Wrong date' 
-    when date(sh.arrival_date) = current_date()+1 then "Tomorrow" 
-    when date(sh.arrival_date) > current_date() then "Future" 
-    when date(sh.arrival_date) = current_date()-1 then "Yesterday" 
-    when date(sh.arrival_date) = current_date() then "Today" 
-    when date_diff(cast(current_date() as date ),cast(sh.arrival_date as date), MONTH) = 0 then 'Month To Date'
-    when date_diff(cast(current_date() as date ),cast(sh.arrival_date as date), MONTH) = 1 then 'Last Month'
-    when date_diff(cast(current_date() as date ),cast(sh.arrival_date as date), YEAR) = 0 then 'Year To Date'
+    when date_diff(date(msh.arrival_date)  ,current_date(), month) > 1 then 'Wrong date' 
+    when date(msh.arrival_date) = current_date()+1 then "Tomorrow" 
+    when date(msh.arrival_date) > current_date() then "Future" 
+    when date(msh.arrival_date) = current_date()-1 then "Yesterday" 
+    when date(msh.arrival_date) = current_date() then "Today" 
+    when date_diff(cast(current_date() as date ),cast(msh.arrival_date as date), MONTH) = 0 then 'Month To Date'
+    when date_diff(cast(current_date() as date ),cast(msh.arrival_date as date), MONTH) = 1 then 'Last Month'
+    when date_diff(cast(current_date() as date ),cast(msh.arrival_date as date), YEAR) = 0 then 'Year To Date'
     else "Past" end as select_arrival_date,
 
 
