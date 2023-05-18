ac91f462ff237a8f5061d3a492dbd9ee5a2f6823
Anmar Abbas <anmar@floranow.com>
2023-05-18 09:09:47 +0000

    Update query `Products`

--- 1. ERP Models/Products/1659460-Products.sql
+++ 1. ERP Models/Products/1659460-Products.sql
@@ -179,7 +179,7 @@ when st.id in (18,,22,23,128,129) then 'Marketplace - Astra Express'
 when st.id in (304,305) then 'Marketplace - Ward'
 when st.id in (14,15) then 'Marketplace - Wish Flowers'
 
-when st.id in (91,90,,,,,) then 'Commission Based - Astra Express'
+when st.id in (91,90,19,,,,) then 'Commission Based - Astra Express'
 when st.id in (417,416,,,,) then 'Reselling Event'
 
 
