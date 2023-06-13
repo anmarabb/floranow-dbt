39b1484e4ab0047a15b08b851ee56e44e13e71a8
Anmar Abbas <anmar@floranow.com>
2023-06-13 12:03:29 +0000

    y

--- models/1_staging/erp_prod/3_inventory/int_products.sql
+++ models/1_staging/erp_prod/3_inventory/int_products.sql
@@ -88,7 +88,7 @@ with CTE as
             li.Shipment,
             li.shipments_status,
             li.master_shipments_status,
-            li.master_shipment_name,
+            li.master_shipment,
             
             case 
             when date_diff(date(case when li.delivery_date is null and li.order_type in ('IMPORT_INVENTORY', 'EXTRA','MOVEMENT') then date(li.order_date) else li.delivery_date end)  ,current_date(), month) > 1 then 'Wrong date' 
