e1020978476fdc5faf442123ac0ad1de4b466b76
Anmar Abbas <anmar@floranow.com>
2023-12-20 14:42:42 +0000

    y

--- models/1_staging/erp_prod/3_inventory/int_product_locations.sql
+++ models/1_staging/erp_prod/3_inventory/int_product_locations.sql
@@ -1,16 +1,25 @@
 With source as (
 
     
- select 
- 
+ select
+ product_name as Product,
+
+ p.warehouse,
 
 pl.locationable_id,
 concat(loc.label, " - ", sec.section_name) as Location,
+
+pl.quantity as location_quantity,
+pl.remaining_quantity as location_remaining_quantity,
+
 
 
 from {{ ref('stg_product_locations') }} as pl
 left join {{ ref('stg_locations')}} as loc on pl.location_id=loc.location_id
 left join {{ ref('stg_sections')}} as sec on sec.section_id = loc.section_id
+
+left join {{ ref('int_products')}} as p on pl.locationable_id = p.product_id
+
 
 where pl.locationable_type = "Product"
  
