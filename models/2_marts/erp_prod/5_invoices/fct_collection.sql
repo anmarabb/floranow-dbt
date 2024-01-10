3755b2e05268edcd85f91097ab36bf320b04698f
Anmar Abbas <anmar@floranow.com>
2024-01-10 08:50:06 +0000

    u

---
+++ models/2_marts/erp_prod/5_invoices/fct_collection.sql
@@ -0,0 +1,13 @@
+
+select 
+    b.date,
+    b.financial_administration,
+    b.account_manager,
+    --b.city,
+    --b.client_category,
+    b.monthly_budget,
+    b.daily_budget,
+    --b.warehouse,
+
+FROM  {{ref('int_collection')}} as b   
+
