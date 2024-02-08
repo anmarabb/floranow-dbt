f3dd0f47df2c331a1ed4be2f354ce5519c831664
Anmar Abbas <anmar@floranow.com>
2024-02-08 08:29:27 +0000

    y

--- models/1_staging/erp_prod/7_farm_management/int_fm_box_items.sql
+++ models/1_staging/erp_prod/7_farm_management/int_fm_box_items.sql
@@ -19,8 +19,11 @@ select
 --shipments
     sh.fm_shipment_id,
     cast(sh.created_at as date) as shipmet_creation_date, 
-    time_add(cast(sh.created_at as time), INTERVAL 3 hour) as shipment_creation_time
+    time_add(cast(sh.created_at as time), INTERVAL 3 hour) as shipment_creation_time,
 
+
+case when sh.shipment_type = 'INBOUND' and sh.status <> 'PENDING' and sh.created_at > '2023-11-11' then 'Production'
+else 'Normal' end as shipment_type,
 
 from   {{ref('stg_fm_box_items')}} as bi
 left join {{ref('stg_fm_boxes')}} as b on bi.fm_box_id = b.fm_boxe_id
@@ -30,4 +33,3 @@ left join {{ref('stg_fm_shipments')}} as sh on b.fm_shipment_id = sh.fm_shipment
 
 left join {{ref('stg_fm_products')}} as p on bi.fm_product_id = p.fm_product_id
 
---where sh.shipment_type = 'INBOUND' and sh.status <> 'PENDING' and sh.created_at > '2023-11-11'
