ea542c3ab89fd1adfba7f3abe9abb1c2e9eb676d
Anmar Abbas <anmar@floranow.com>
2024-02-09 13:42:35 +0000

    y

--- models/2_marts/erp_prod/3_inventory/fct_products.sql
+++ models/2_marts/erp_prod/3_inventory/fct_products.sql
@@ -284,7 +284,7 @@ STDDEV_POP(sold_quantity) over (partition by p.product_name, p.warehouse) AS sol
 
 case 
     when loc_status = 'null' and shipments_status in ('PACKED', 'WAREHOUSED') and order_status = 'Not Fulfilled' then 'Received Not Scanned'
-    when loc_status = 'loc' and shipments_status not null then 'Received On Location'
+    when loc_status = 'loc' and shipments_status is not null then 'Received On Location'
     else 'To Be Scoped'
     end as orders_progress, 
 
