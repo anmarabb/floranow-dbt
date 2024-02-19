df5470f7b0f322eab3b63641521d1167e0b3252b
Anmar Abbas <anmar@floranow.com>
2024-02-19 07:45:47 +0000

    y

--- models/1_staging/erp_prod/7_farm_management/int_astra_budget_2024.sql
+++ models/1_staging/erp_prod/7_farm_management/int_astra_budget_2024.sql
@@ -33,8 +33,10 @@ with produced_quantity as
 SELECT
 
 db.*,
-boxitems.produced_quantity,
-p.available_quantity,
+
+COALESCE(boxitems.produced_quantity, 0) AS produced_quantity,
+COALESCE(p.available_quantity, 0) AS available_quantity,
+
 FROM  {{ref('stg_astra_budget_2024')}} as db
 left join produced_quantity as boxitems on boxitems.sub_group = db.sub_group and boxitems.color = db.color and  boxitems.week_number = db.week_number
 left join available_quantity as p on p.sub_group = db.sub_group and p.color = db.color
