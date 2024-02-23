3948570202435dfa0f7014cadbb32d4f76674447
Anmar Abbas <anmar@floranow.com>
2024-02-23 14:25:02 +0000

    y

---
+++ models/2_marts/erp_prod/7_farm_management/fct_fm_stock_transactions.sql
@@ -0,0 +1,16 @@
+select
+
+
+stock_transaction_at,
+production_date,
+expired_at,
+
+--product
+    sub_group,
+    color,
+    available_quantity,
+    contract_status,
+    bud_count,
+    stem_length,
+    product_name,
+from   {{ ref('int_fm_stock_transactions') }} as p
