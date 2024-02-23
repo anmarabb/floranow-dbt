d2926d8bb55c59e699f37bf6957d654af23d745d
Anmar Abbas <anmar@floranow.com>
2024-02-23 14:07:44 +0000

    y

--- models/1_staging/erp_prod/7_farm_management/stg_astra_budget_2024.sql
+++ models/1_staging/erp_prod/7_farm_management/stg_astra_budget_2024.sql
@@ -12,4 +12,12 @@ SELECT
 
 
 
+CASE
+    WHEN sub_group IN ('Alstroemeria', 'Aster', 'Cycas', 'Eucalyptus', 'Eustoma', 'Liatris', 'Gerbera', 'Trachelium', 'Sunflower', 'Statice', 'Solidago', 'Ruscus', 'Lily Or Double', 'Lily Or', 'Lily LA', 'Chrysanthemum Santini', 'Chrysanthemum Single', 'Chrysanthemum Spray') THEN 'Contract'
+    WHEN sub_group IN ('Antirrhinum', 'Carnation', 'Dianthus barbatus', 'Gypsophila', 'Rose', 'Spray Rose', 'Celosia', 'Greeneries') THEN 'Out Of Contract'
+    ELSE 'Unknown'
+  END AS contract_status,
+
+
+
     FROM {{ source(var('erp_source'), 'astra_budget_2024') }} as b
