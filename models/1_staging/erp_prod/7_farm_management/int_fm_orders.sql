8ff63032a53547ddf7a33ec2c72f44fb5c70a4c1
Anmar Abbas <anmar@floranow.com>
2024-04-11 21:08:41 +0000

    y

--- models/1_staging/erp_prod/7_farm_management/int_fm_orders.sql
+++ models/1_staging/erp_prod/7_farm_management/int_fm_orders.sql
@@ -23,12 +23,17 @@ with
             sum(bi.packed_quantity) as packed_quantity,
             sum(bi.unpacked_quantity) as unpacked_quantity,
             max(order_status) as order_status,
-            bi.fm_order_id  
+            --(bi.fm_box_id) as fm_box_id,
+            bi.fm_order_id,
+              
             
          from   {{ ref('stg_fm_box_items') }} as bi 
          group by fm_order_id
 
     ),
+
+
+
 
 
     prep_outbound_stock_items as (
@@ -120,10 +125,10 @@ left join {{ ref('stg_line_items') }} as li on o.buyer_order_number = li.number
 
 
 --left join {{ ref('stg_fm_shipments') }} as sh on sh.fm_shipment_id = o.fm_shipment_id
+
 left join {{ref('base_users')}} as customer on customer.debtor_number = o.customer_debtor_number
 
 left join prep_box_items as bi on bi.fm_order_id = o.fm_order_id
-
 
 left join prep_outbound_stock_items as osi on osi.fm_order_id = o.fm_order_id
 
