a9f63ec107fc334951ffafd8bfb207f75d776dcc
Nourhan Abu Zannad <nabuzannad@floranow.com>
2024-08-18 13:51:34 +0000

    -

--- models/2_marts/erp_prod/8_project_x/dim_picked_items.sql
+++ models/2_marts/erp_prod/8_project_x/dim_picked_items.sql
@@ -1,3 +1,12 @@
-SELECT *
+SELECT  
+       pi.*, 
+       p.Supplier,
+       p.warehouse,
+       p.Product,
+       p.order_type,
+       concat(loc.label, " - ", sec.section_name) as Location,
+from {{ref ('stg_picked_items')}} pi
+left join {{ref ('fct_products')}} p on pi.product_id = p.product_id
+left join {{ ref('stg_locations')}} as loc on pi.location_id=loc.location_id
+left join {{ ref('stg_sections')}} as sec on sec.section_id = loc.section_id
 
-from {{ref ('stg_picked_items')}}
