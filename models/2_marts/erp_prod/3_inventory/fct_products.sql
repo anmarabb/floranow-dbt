eb8215d1f57070db0dac33ee2570c67f1f823b89
Nourhan Abu Zannad <nabuzannad@floranow.com>
2024-08-22 05:58:53 +0000

    -

--- models/2_marts/erp_prod/3_inventory/fct_products.sql
+++ models/2_marts/erp_prod/3_inventory/fct_products.sql
@@ -29,7 +29,7 @@ select
 
 case 
   when Stock = 'Inventory Stock' and live_stock = 'Live Stock' and stock_model in ('Reselling', 'Commission Based') and flag_1 in ('scaned_flag', 'scaned_good') then 'Current Inventory' 
-  when Stock = 'Inventory Stock' and live_stock = 'Live Stock' and stock_model in ('Internal') and flag_1 in ('scaned_flag', 'scaned_good') then 'Internal - Current Inventory'
+  when Stock = 'Inventory Stock' and live_stock = 'Live Stock' and stock_model in ('Internal', 'Internal - Project X') and flag_1 in ('scaned_flag', 'scaned_good') then 'Internal - Current Inventory'
   else null 
   end as report_filter,
 
