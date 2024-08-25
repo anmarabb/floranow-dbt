93d098eef9fc56a58a2fbc174f98036826b97e7e
Nourhan Abu Zannad <nabuzannad@floranow.com>
2024-08-25 08:16:49 +0000

    -

--- models/1_staging/erp_prod/1_orders/int_line_items.sql
+++ models/1_staging/erp_prod/1_orders/int_line_items.sql
@@ -116,44 +116,44 @@ case when li.departure_date is null then date(li.created_at) else li.departure_d
 
 
 
--- case when li.li_record_type_details in ('Reseller Purchase Order For Inventory') and li.location = 'loc' and pi.incidents_count is  null then 1 else 0 end as Received_not_scanned,
+case when li.li_record_type_details in ('Reseller Purchase Order For Inventory') and li.location = 'loc' and pi.incidents_count is  null then 1 else 0 end as Received_not_scanned,
 
 --actions
-    --returned_by.name as returned_by,
-    -- dispatched_by.name as dispatched_by,
-    --created_by.name as created_by,
-    --split_by.name as split_by,
-    --order_requested_by.name as order_requested_by,
+    returned_by.name as returned_by,
+    dispatched_by.name as dispatched_by,
+    created_by.name as created_by,
+    split_by.name as split_by,
+    order_requested_by.name as order_requested_by,
 
 
 
 
 -- --funnel touchpoints 
---     case when li.received_quantity > 0 then 1 else 0 end as order_received,
---     case when li.fulfilled_quantity > 0 then 1 else 0 end as order_fulfilled,
---     case when li.location = 'pod' then 1 else 0 end as order_pod_moved,
+    case when li.received_quantity > 0 then 1 else 0 end as order_received,
+    case when li.fulfilled_quantity > 0 then 1 else 0 end as order_fulfilled,
+    case when li.location = 'pod' then 1 else 0 end as order_pod_moved,
 
 
 
-    -- case when li.state = 'DELIVERED' then 1 else 0 end as order_delivered,
-    -- case when li.invoice_id is not null then 1 else 0 end as invoice_created,
-    -- case when li.invoice_id is not null and i.invoice_header_printed_at is not null then 1 else 0 end as invoice_printed,
+    case when li.state = 'DELIVERED' then 1 else 0 end as order_delivered,
+    case when li.invoice_id is not null then 1 else 0 end as invoice_created,
+    case when li.invoice_id is not null and i.invoice_header_printed_at is not null then 1 else 0 end as invoice_printed,
 
 
-    -- case when li.location = 'loc' then 1 else 0 end as order_loc_moved, --order_warehoused
-    --case when li.picked_quantity > 0 then 1 else 0 end as order_picked,
+    case when li.location = 'loc' then 1 else 0 end as order_loc_moved, --order_warehoused
+    -- case when li.picked_quantity > 0 then 1 else 0 end as order_picked,
 
 
 
 
 
 --date
-    -- date.dim_date,
+    date.dim_date,
     
 
 
 --customer
-    -- user.name as user,
+    user.name as user,
     case when li.reseller_id is not null then  'Reseller' else customer.name  end as customer,
     customer.country,
     customer.financial_administration,
@@ -174,11 +174,11 @@ case when li.departure_date is null then date(li.created_at) else li.departure_d
 
     case when customer.debtor_number in ('WANDE','95110') then 'Internal Invoicing' else 'Normal Invoicing' end as internal_invoicing,
         -- li.dispatched_at,
-    -- case when li.received_quantity > 0 then 'Received' else 'Not Received' end as ops_status1,
-    -- case when li.state in ('PENDING','CANCELED') then 'Not Fulfilled' else 'Fulfilled' end as ops_status2,
-    -- case when li.location = 'pod' then 'Prepared' else 'Not Prepared' end as ops_status3,
-    -- case when li.dispatched_at is not null then 'Dispatched' else 'Not Dispatched' end as ops_status4,
-    -- case when li.state = 'DELIVERED' then 'Signed' else 'Not Signed' end as ops_status5,
+    case when li.received_quantity > 0 then 'Received' else 'Not Received' end as ops_status1,
+    case when li.state in ('PENDING','CANCELED') then 'Not Fulfilled' else 'Fulfilled' end as ops_status2,
+    case when li.location = 'pod' then 'Prepared' else 'Not Prepared' end as ops_status3,
+    case when li.dispatched_at is not null then 'Dispatched' else 'Not Dispatched' end as ops_status4,
+    case when li.state = 'DELIVERED' then 'Signed' else 'Not Signed' end as ops_status5,
 
 
     concat( "https://erp.floranow.com/line_items/", li.line_item_id) as line_item_link,
@@ -213,8 +213,8 @@ case when li.departure_date is null then date(li.created_at) else li.departure_d
 --order requist
     orr.status as order_request_status,
     concat( "https://erp.floranow.com/order_requests/", li.order_request_id) as order_request_link,
-    -- case when li.order_request_id is not null then 'Order Request ID' else null end as order_request_cheack,
-    -- case when li.replaced_quantity is not null then 'Replaced Qty.' else null end as replaced_quantity_cheack,
+    case when li.order_request_id is not null then 'Order Request ID' else null end as order_request_cheack,
+    case when li.replaced_quantity is not null then 'Replaced Qty.' else null end as replaced_quantity_cheack,
 
 
     --orr.quantity as requested_quantity,
@@ -222,8 +222,8 @@ case when li.departure_date is null then date(li.created_at) else li.departure_d
 
 
 --order_payloads
-    --opl.offer_id,
-    -- opl.status as order_payloads_status,
+    opl.offer_id as order_offer_id,
+    opl.status as order_payloads_status,
 
 
 --shipments
@@ -290,14 +290,14 @@ COALESCE(pi.incident_orders_after_return_stage, null) as incident_orders_after_r
 
 
 
--- pod.source_type,
--- pod.pod_status,
--- pod.route_name,
+pod.source_type,
+pod.pod_status,
+pod.route_name,
 
--- case 
---     when pod.route_name in ('Ajman', 'Sharjah', 'Northern Emirates', 'Ras Al Khaimah', 'Umm Al Quwain')  then 'Northern Emirates'
---     when pod.route_name in ('Al Ain' ,'Al Ain 1') then 'Al Ain City'
---     else pod.route_name end as vehicle_destination,
+case 
+    when pod.route_name in ('Ajman', 'Sharjah', 'Northern Emirates', 'Ras Al Khaimah', 'Umm Al Quwain')  then 'Northern Emirates'
+    when pod.route_name in ('Al Ain' ,'Al Ain 1') then 'Al Ain City'
+    else pod.route_name end as vehicle_destination,
 
 
 
@@ -323,10 +323,10 @@ win.name as delivery_window,
 
 
 
--- case 
--- when customer.debtor_number  in ('SHPRUH','LNDHAI','ASTHAI','LNDJOU','ASTJOU','LNDHAF','ASTHAF','LNDQAS','ASTQAS','lndmed','astmed','EVEDMM', 'EVEHAF','EVEHAI','EVEJED','EVEJOU','EVEMED','EVEQAS','EVERUH','EVETUU','LNDDMM','ASTJED','LNDJED','FNQSIM','ASTRUH','LNDRUH','FSTUU') then 'BMX Reseller'
--- when customer.debtor_number  in ('130220','130009','130257','130188','132009','132008','134151','134150','820762') then 'BMX Original'
--- else null end as ksa_resellers,
+case 
+when customer.debtor_number  in ('SHPRUH','LNDHAI','ASTHAI','LNDJOU','ASTJOU','LNDHAF','ASTHAF','LNDQAS','ASTQAS','lndmed','astmed','EVEDMM', 'EVEHAF','EVEHAI','EVEJED','EVEJOU','EVEMED','EVEQAS','EVERUH','EVETUU','LNDDMM','ASTJED','LNDJED','FNQSIM','ASTRUH','LNDRUH','FSTUU') then 'BMX Reseller'
+when customer.debtor_number  in ('130220','130009','130257','130188','132009','132008','134151','134150','820762') then 'BMX Original'
+else null end as ksa_resellers,
 
 
   case 
@@ -336,42 +336,42 @@ win.name as delivery_window,
     end as invoice_status,
 
 
--- case when li.line_item_id is not null then 'Line Item ID' else null end as line_item_id_check,
--- case when li.order_id is not null then 'Order ID' else null end as order_id_check,
--- case when li.order_number is not null then 'Order Number ID' else null end as order_number_check,
+case when li.line_item_id is not null then 'Line Item ID' else null end as line_item_id_check,
+case when li.order_id is not null then 'Order ID' else null end as order_id_check,
+case when li.order_number is not null then 'Order Number ID' else null end as order_number_check,
 
--- case when li.root_shipment_id is not null then 'Root Shipment ID' else null end as root_shipment_id_check,
+case when li.root_shipment_id is not null then 'Root Shipment ID' else null end as root_shipment_id_check,
 
 
 
--- case when li.shipment_id is not null then 'Shipment ID' else null end as shipment_id_check,
--- case when li.invoice_id is not null then 'Invoice ID' else null end as invoice_id_check,
+case when li.shipment_id is not null then 'Shipment ID' else null end as shipment_id_check,
+case when li.invoice_id is not null then 'Invoice ID' else null end as invoice_id_check,
 
--- case 
--- when li.invoice_id is null then null 
--- when li.invoice_id is not null and i.invoice_header_printed_at is null then null 
--- else 'Invoice Number' end as invoice_number_check,
+case 
+when li.invoice_id is null then null 
+when li.invoice_id is not null and i.invoice_header_printed_at is null then null 
+else 'Invoice Number' end as invoice_number_check,
 
 
 case when li.parent_line_item_id is not null then 'Parent ID' else null end as parent_id_check,
 
--- case when ppli.line_item_id is not null then 'Parent Parent ID' else null end as parent_parent_id_check,
+case when ppli.line_item_id is not null then 'Parent Parent ID' else null end as parent_parent_id_check,
 
 
 
--- case when li.source_line_item_id is not null then 'Source ID' else null end as source_id_check,
--- case when p.line_item_id is not null then 'Product ID' else null end as product_id_check,
--- case when li.offer_id is not null then 'Offer ID' else null end as offer_id_check,
+case when li.source_line_item_id is not null then 'Source ID' else null end as source_id_check,
+case when p.line_item_id is not null then 'Product ID' else null end as product_id_check,
+case when li.offer_id is not null then 'Offer ID' else null end as offer_id_check,
 
--- case when li.reseller_id is not null then 'Reseller ID' else null end as reseller_id_check,
--- case when li.customer_id is not null then 'Customer ID' else null end as customer_id_check,
--- case when li.supplier_id is not null then 'Supplier ID' else null end as supplier_id_check,
+case when li.reseller_id is not null then 'Reseller ID' else null end as reseller_id_check,
+case when li.customer_id is not null then 'Customer ID' else null end as customer_id_check,
+case when li.supplier_id is not null then 'Supplier ID' else null end as supplier_id_check,
 
--- case when li.reseller_id = li.customer_id then 'RC ID' else null end as reseller_customer_id_check,
+case when li.reseller_id = li.customer_id then 'RC ID' else null end as reseller_customer_id_check,
 
 
--- case when li.customer_master_id is not null then 'Master ID' else null end as customer_master_id_check,
--- case when li.proof_of_delivery_id is not null then 'POD ID' else null end as proof_of_delivery_id_check,
+case when li.customer_master_id is not null then 'Master ID' else null end as customer_master_id_check,
+case when li.proof_of_delivery_id is not null then 'POD ID' else null end as proof_of_delivery_id_check,
 
 
 
@@ -381,12 +381,12 @@ p.product_id,
 concat( "https://erp.floranow.com/products/", p.product_id) as product_link,
 concat( "https://erp.floranow.com/line_items/", li.parent_line_item_id) as parent_line_item_link,
 
--- case 
--- when customer.debtor_number in ('130009','ASTHAI','EVEHAI','LNDHAI') then 'Hail'
--- WHEN customer.debtor_number in ('130220', 'ASTJOU', 'LNDJOU','EVEJOU') then 'Jouf'
--- WHEN customer.debtor_number in ('132009','ASTHAF','LNDHAF','EVEHAF') then 'Hafer'
--- WHEN customer.debtor_number in ('130257','LNDQAS','EVEQAS','FNQIM') then 'Qaseem'
--- else null end as samer_warehouses,
+case 
+when customer.debtor_number in ('130009','ASTHAI','EVEHAI','LNDHAI') then 'Hail'
+WHEN customer.debtor_number in ('130220', 'ASTJOU', 'LNDJOU','EVEJOU') then 'Jouf'
+WHEN customer.debtor_number in ('132009','ASTHAF','LNDHAF','EVEHAF') then 'Hafer'
+WHEN customer.debtor_number in ('130257','LNDQAS','EVEQAS','FNQIM') then 'Qaseem'
+else null end as samer_warehouses,
 
 
 
@@ -395,16 +395,16 @@ fs.feed_source_name,
 fs.feed_type,
 fs.supplier_name as feed_source_supplier,
 reseller.name as Reseller,
--- master.name as Master,
+master.name as Master,
 
 ii.quantity as inv_quantity,
 ii.quantity * ii.unit_price as inv_total_price_without_tax,
 
--- ad.status as additional_status, 
--- ad.creation_stage as additional_creation_stage,
--- ad.additional_items_report_id,
--- concat( "https://erp.floranow.com/additional_items_reports/", ad.additional_items_report_id) as additional_item_link,
--- case when ad.additional_items_report_id is not null then 'Additional ID' else null end as additional_id_check,
+ad.status as additional_status, 
+ad.creation_stage as additional_creation_stage,
+ad.additional_items_report_id,
+concat( "https://erp.floranow.com/additional_items_reports/", ad.additional_items_report_id) as additional_item_link,
+case when ad.additional_items_report_id is not null then 'Additional ID' else null end as additional_id_check,
 
 concat( "https://erp.floranow.com/line_items/", li.source_line_item_id) as source_line_item_link,
 
@@ -456,31 +456,31 @@ else st.stock_model_details end as stock_model_details,
 
 
 
--- case when li.order_source = 'Direct Supplier' then 1 else 0 end as direct_line_order_count,
--- case when li.order_source = 'Express Inventory' then 1 else 0 end as stock_line_order_count,
+case when li.order_source = 'Direct Supplier' then 1 else 0 end as direct_line_order_count,
+case when li.order_source = 'Express Inventory' then 1 else 0 end as stock_line_order_count,
 
--- case when li.order_source = 'Direct Supplier' then li.order_id else null end as direct_order_ids,
--- case when li.order_source = 'Express Inventory' then li.order_id else null end as stock_order_ids,
+case when li.order_source = 'Direct Supplier' then li.order_id else null end as direct_order_ids,
+case when li.order_source = 'Express Inventory' then li.order_id else null end as stock_order_ids,
 
 
--- case when li.fulfillment_status = 'Fulfilled' then 1 else 0 end as fulfilled_items,
--- case when li.dispatched_status = 'Dispatched' then 1 else 0 end as dispatched_items,
--- case when li.signed_status = 'Signed' then 1 else 0 end as signed_items,
+case when li.fulfillment_status = 'Fulfilled' then 1 else 0 end as fulfilled_items,
+case when li.dispatched_status = 'Dispatched' then 1 else 0 end as dispatched_items,
+case when li.signed_status = 'Signed' then 1 else 0 end as signed_items,
 
 
 concat(st.stock_id, " - ", st.stock_name, " - ", reseller.name  ) as full_stock_name,
 
 
--- pp.product_id as parent_product_id,
+pp.product_id as parent_product_id,
 
 
 
--- CASE 
---   WHEN li.ordering_stock_type IS NOT NULL AND pp.product_id = SAFE_CAST(li.offer_id AS INT64) THEN 'ordering from reselling stocks product - GOOD'
---   WHEN li.ordering_stock_type IS NOT NULL AND pp.product_id != SAFE_CAST(li.offer_id AS INT64) THEN 'ordering from reselling stocks product - To Be Scoped'
---   WHEN li.ordering_stock_type IS NULL THEN 'ordering from external supplier offer'
---   ELSE 'To Be Scoped'
--- END AS ordering_source_details,
+CASE 
+  WHEN li.ordering_stock_type IS NOT NULL AND pp.product_id = SAFE_CAST(li.offer_id AS INT64) THEN 'ordering from reselling stocks product - GOOD'
+  WHEN li.ordering_stock_type IS NOT NULL AND pp.product_id != SAFE_CAST(li.offer_id AS INT64) THEN 'ordering from reselling stocks product - To Be Scoped'
+  WHEN li.ordering_stock_type IS NULL THEN 'ordering from external supplier offer'
+  ELSE 'To Be Scoped'
+END AS ordering_source_details,
 
 sh.master_shipment_id,
 
@@ -489,10 +489,10 @@ li.supplier_product_name as supplier_product,
 li.colour as product_color,
 
 case when li.parent_line_item_id is not null then pli.raw_unit_fob_price else li.raw_unit_fob_price end as unit_fob_price,
--- case when li.parent_line_item_id is not null then pli.raw_fob_currency else li.raw_fob_currency end as fob_currency,
+case when li.parent_line_item_id is not null then pli.raw_fob_currency else li.raw_fob_currency end as fob_currency,
 
 
--- fmo.production_date_array,
+fmo.production_date_array,
 
 
 
@@ -501,34 +501,34 @@ case when li.parent_line_item_id is not null then pli.raw_unit_fob_price else li
 from {{ref('stg_line_items')}} as li
 left join {{ ref('stg_products') }} as p on p.line_item_id = li.line_item_id 
 left join {{ref('stg_order_requests')}} as orr on li.order_request_id = orr.id
--- left join {{ref('stg_order_payloads')}} as opl on li.order_payload_id = opl.order_payload_id
+left join {{ref('stg_order_payloads')}} as opl on li.order_payload_id = opl.order_payload_id
 left join {{ref('stg_invoice_items')}} as ii on ii.line_item_id=li.line_item_id and ii.invoice_item_type = 'invoice'
---left join {{ref('stg_invoice_items')}} as ii2 on ii2.line_item_id=li.line_item_id and ii2.invoice_item_type = 'credit note'
+-- left join {{ref('stg_invoice_items')}} as ii2 on ii2.line_item_id=li.line_item_id and ii2.invoice_item_type = 'credit note'
 left join {{ref('stg_invoices')}} as i on li.invoice_id = i.invoice_header_id
--- left join {{ref('base_users')}} as master on master.id = li.customer_master_id
+left join {{ref('base_users')}} as master on master.id = li.customer_master_id
 left join {{ref('base_users')}} as customer on customer.id = li.customer_id
 left join {{ref('base_users')}} as reseller on reseller.id = li.reseller_id
--- left join {{ref('base_users')}} as user on user.id = li.user_id
--- left join {{ref('base_users')}} as dispatched_by on dispatched_by.id = li.dispatched_by_id
--- left join {{ref('base_users')}} as returned_by on returned_by.id = li.returned_by_id
--- left join {{ref('base_users')}} as created_by on created_by.id = li.created_by_id
--- left join {{ref('base_users')}} as split_by on split_by.id = li.split_by_id
--- left join {{ref('base_users')}} as order_requested_by on order_requested_by.id = orr.created_by_id
+left join {{ref('base_users')}} as user on user.id = li.user_id
+left join {{ref('base_users')}} as dispatched_by on dispatched_by.id = li.dispatched_by_id
+left join {{ref('base_users')}} as returned_by on returned_by.id = li.returned_by_id
+left join {{ref('base_users')}} as created_by on created_by.id = li.created_by_id
+left join {{ref('base_users')}} as split_by on split_by.id = li.split_by_id
+left join {{ref('base_users')}} as order_requested_by on order_requested_by.id = orr.created_by_id
 left join {{ref('base_suppliers')}} as lis on lis.supplier_id = li.supplier_id
--- left join {{ ref('stg_products') }} as pp on pp.line_item_id = li.parent_line_item_id 
+left join {{ ref('stg_products') }} as pp on pp.line_item_id = li.parent_line_item_id 
 left join {{ref('stg_line_items')}} as pli on pli.line_item_id = li.parent_line_item_id
--- left join {{ref('stg_line_items')}} as ppli on ppli.line_item_id = pli.parent_line_item_id
+left join {{ref('stg_line_items')}} as ppli on ppli.line_item_id = pli.parent_line_item_id
 left join {{ref('base_suppliers')}} as plis on plis.supplier_id = pli.supplier_id
--- left join {{ ref('dim_proof_of_deliveries') }} as pod on li.proof_of_delivery_id = pod.proof_of_delivery_id
+left join {{ ref('dim_proof_of_deliveries') }} as pod on li.proof_of_delivery_id = pod.proof_of_delivery_id
 left join {{ref('stg_shipments')}} as sh on li.shipment_id = sh.shipment_id
 left join  {{ref('stg_master_shipments')}} as msh on sh.master_shipment_id = msh.master_shipment_id
 left join {{ref('base_stocks')}} as st on p.stock_id = st.stock_id and p.reseller_id = st.reseller_id
 left join {{ref('stg_feed_sources')}} as fs on fs.feed_source_id = li.feed_source_id
 left join {{ref('base_warehouses')}} as w on w.warehouse_id = customer.warehouse_id
--- left join {{ref('stg_additional_items_reports')}}  as ad on ad.line_item_id=li.line_item_id
--- left join {{ref('dim_date')}}  as date on date.dim_date = date(li.created_at)
+left join {{ref('stg_additional_items_reports')}}  as ad on ad.line_item_id=li.line_item_id
+left join {{ref('dim_date')}}  as date on date.dim_date = date(li.created_at)
 left join {{ref('stg_delivery_windows')}}  as win on  CAST(li.delivery_window_id AS INT64) = win.id
 left join productIncidents as pi on pi.line_item_id = li.line_item_id
--- left join {{ref('int_fm_orders')}}  as fmo on  fmo.buyer_order_number = li.number
+left join {{ref('int_fm_orders')}}  as fmo on  fmo.buyer_order_number = li.number
 
 left join PackageLineItems on li.line_item_id = PackageLineItems.line_item_id
