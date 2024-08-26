3d93365d3d1ed1c2f3229e942a20b18d2de070a5
Nourhan Abu Zannad <nabuzannad@floranow.com>
2024-08-26 12:30:37 +0000

    -

--- models/1_staging/erp_prod/8_project_x/stg_category_mqs.sql
+++ models/1_staging/erp_prod/8_project_x/stg_category_mqs.sql
@@ -1,12 +1,9 @@
 With source as (
- select * from {{ source(var('erp_source'), 'item_movement_requests') }}  )
-select 
-*,
+ select * from {{ source(var('erp_source'), 'category_mqs') }}  )
+
+select *,
 
 
 current_timestamp() as ingestion_timestamp,
  
-
-
-
 from source
