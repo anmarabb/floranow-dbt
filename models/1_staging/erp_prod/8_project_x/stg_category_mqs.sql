4e0860ef8eaa0783b51189f9a4ad84e72658139a
Nourhan Abu Zannad <nabuzannad@floranow.com>
2024-08-26 12:53:12 +0000

    -

--- models/1_staging/erp_prod/8_project_x/stg_category_mqs.sql
+++ models/1_staging/erp_prod/8_project_x/stg_category_mqs.sql
@@ -1,7 +1,11 @@
 With source as (
  select * from {{ source(var('erp_source'), 'category_mqs') }}  )
 
-select *,
+select INITCAP(main_group) as sub_category,
+       sub_group,
+       Product,
+       product_color,
+       MQS,
 
 
 current_timestamp() as ingestion_timestamp,
