e34d7eb046fd80a31be75e451af26a578cf90983
Nourhan Abu Zannad <nabuzannad@floranow.com>
2024-09-03 08:32:52 +0000

    -

--- models/2_marts/erp_prod/8_project_x/fct_project_X_grouped.sql
+++ models/2_marts/erp_prod/8_project_x/fct_project_X_grouped.sql
@@ -93,7 +93,7 @@ left join product_location as pl on p.product_id = pl.locationable_id
 -- left join pi on pi.line_item_id = li.line_item_id
 left join invoices as ii on ii.parent_line_item_id = li.line_item_id 
 -- left join stock_movement as sm on sm.product_id = li.product_id
-left join targets as t on t.Product = li.Product and li.warehouse = t.warehouse
+-- left join targets as t on t.Product = li.Product and li.warehouse = t.warehouse
 left join category_target ct on ct.category_linking = li.li_category_linking
 
 where li.Reseller in ('RUH Project X Stock', 'DMM Project X Stock') and li.order_type != "PICKED_ORDER" --and li.Product = 'Rose Athena'
