0448410c438228cc9e509b618ff3f7a71afc958a
Nourhan Abu Zannad <nabuzannad@floranow.com>
2024-09-09 07:31:14 +0000

    -

--- models/2_marts/erp_prod/fct_daily_overview.sql
+++ models/2_marts/erp_prod/fct_daily_overview.sql
@@ -7,6 +7,11 @@ with invoices_daily as (
         i.financial_administration,
 
         COALESCE(SUM(i.gross_revenue + i.credit_note), 0) as Sales,
+
+        COALESCE(SUM(i.auto_gross_revenue), 0) as auto_gross_revenue,
+        COALESCE(SUM(i.auto_credit_note), 0) as auto_credit_note,
+        COALESCE(SUM(i.total_cost), 0) as total_cost,
+
         FROM {{ ref('fct_invoices') }} i
         where i.reprot_filter = 'Floranow Sales'
         GROUP BY 1,2,3
@@ -26,7 +31,23 @@ with invoices_daily as (
         --and Supplier != 'ASTRA Farms'
         GROUP BY 1,2,3
 
-        )
+        ),
+     
+     product_quantity as (
+
+      select 
+      current_date() as date,
+      p.warehouse,
+      p.financial_administration,
+
+      COALESCE(SUM(in_stock_quantity), 0) as in_stock_quantity,
+      COALESCE(SUM(remaining_value), 0) as inventory_value,
+
+      from {{ref("fct_products")}} p
+      group by 1, 2, 3
+
+     )
+
 SELECT 
     d.date,
     d.warehouse,
@@ -36,10 +57,18 @@ SELECT
     COALESCE(SUM(pi.Dmaged), 0) as Dmaged,
 
 
+    COALESCE(SUM(i.auto_gross_revenue), 0) as auto_gross_revenue,
+    COALESCE(SUM(i.auto_credit_note), 0) as auto_credit_note,
+    COALESCE(SUM(i.total_cost), 0) as total_cost,
+
+    COALESCE(SUM(pq.in_stock_quantity), 0) as in_stock_quantity,
+    COALESCE(SUM(pq.inventory_value), 0) as inventory_value
+
+
 FROM  {{ ref('stg_daily_overview') }} as d
 left JOIN  incidents_daily as pi ON date(d.date) = date(pi.incident_at) and d.warehouse =  pi.warehouse and d.financial_administration =pi.financial_administration
 left JOIN  invoices_daily as i ON date(d.date) = date(i.invoice_header_printed_at) and d.warehouse = i.warehouse and d.financial_administration = i.financial_administration
-
+left JOIN  product_quantity as pq ON date(d.date) = date(pq.date) and d.warehouse = pq.warehouse and d.financial_administration = pq.financial_administration
 GROUP BY 
     1,2,3
 
