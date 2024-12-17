ff648001f0b3144a6ec241b6fd674355366516fc
Nourhan Abu Zannad <nabuzannad@floranow.com>
2024-12-17 11:13:01 +0000

    created_by_name

--- models/1_staging/erp_prod/5_invoices/int_payment_transactions.sql
+++ models/1_staging/erp_prod/5_invoices/int_payment_transactions.sql
@@ -12,13 +12,13 @@ customer.debtor_number,
 customer.company_name,
 customer.city,
 customer.user_category,
+created_by.name as created_by_name,
 
-
-    current_timestamp() as insertion_timestamp, 
+current_timestamp() as insertion_timestamp, 
 
 from {{ ref('stg_payment_transactions')}} as pt
 left join {{ ref('base_users') }} as customer on customer.id = pt.user_id
-
+left join {{ ref('base_users') }} as created_by on created_by.id = pt.payment_received_by
 
 
 
