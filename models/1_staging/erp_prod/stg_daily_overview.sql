184c3a625dbc9ced9a74983f6c4a06325341ac04
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-01-15 07:33:00 +0000

    -

--- models/1_staging/erp_prod/stg_daily_overview.sql
+++ models/1_staging/erp_prod/stg_daily_overview.sql
@@ -1,49 +1,14 @@
-WITH date_series AS (
-    SELECT d AS date
-    FROM UNNEST(GENERATE_DATE_ARRAY('2022-01-01', '2025-01-01', INTERVAL 1 DAY)) AS d
-),
-warehouse_list AS (
-    SELECT warehouse
-    FROM UNNEST([
-        'Riyadh Warehouse',
-        'Dubai Warehouse',
-        'Dammam Warehouse',
-        'Jouf WareHouse',
-        'Medina Warehouse',
-        'Jeddah Warehouse',
-        'Hafar WareHouse',
-        'Qassim Warehouse',
-        'Hail Warehouse',
-        'Tabuk Warehouse',
-        'Riyadh Project X',
-        'Dammam Project X'
+SELECT supplier_region as Origin,
+       Supplier,
+       product_name as Product,
+       invoice_header_printed_at,
+       delivery_date,
+       --order_date,
+       financial_administration,
+       warehouse,
+       line_item_id, 
 
-
-    ]) AS warehouse
-),
-
-financial_administration_list AS (
-    SELECT financial_administration
-    FROM UNNEST([
-        'KSA',
-        'UAE',
-        'Internal',
-        'Kuwait',
-        'Bulk',
-        'Qatar'
-    
-    ]) AS financial_administration
-)
-SELECT 
-    date_series.date,
-    financial_administration_list.financial_administration,
-    warehouse_list.warehouse,
-FROM 
-    date_series
-CROSS JOIN 
-    warehouse_list
-CROSS JOIN 
-    financial_administration_list
+FROM {{ref ("int_line_items")}}
 
 
 
