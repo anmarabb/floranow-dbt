bf1608fa7104ef8fb167187c4613846b442d28f4
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-05-08 09:21:06 +0000

    -

---
+++ models/2_marts/erp_prod/fct_training_data.sql
@@ -0,0 +1,7 @@
+select t.warehouse, t.product, date(cli.created_at) as order_date, sum(cli.ordered_quantity) as sold_quantity
+from {{ref ("stg_training_data")}} t
+left join {{ref ("int_line_items")}} li on li.line_item_id = t.line_item_id
+left join {{ref ("int_line_items")}} cli on cli.parent_line_item_id = li.line_item_id
+where cli.customer_type = 'retail' -- and p.warehouse = 'Dubai Warehouse' 
+and t.product in ('Rose Ever Red', 'Rose Athena', 'Chrysanthemum Spray Pina Colada', 'Gypsophila Xlence', 'Rose Madam Red')
+group by 1,2,3
