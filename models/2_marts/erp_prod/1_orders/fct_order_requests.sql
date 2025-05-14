1186cca254ea3369189971ef8e6272bdf7367b6c
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-05-14 08:17:17 +0000

    cancelled_quantity

--- models/2_marts/erp_prod/1_orders/fct_order_requests.sql
+++ models/2_marts/erp_prod/1_orders/fct_order_requests.sql
@@ -1,4 +1,4 @@
 select *,
        supplier_region as Origin,
-       quantity - confirmed_quantity as cancelled_quantity,
+       quantity - ordered_quantity as cancelled_quantity,
 from {{ref("int_order_requests")}}
