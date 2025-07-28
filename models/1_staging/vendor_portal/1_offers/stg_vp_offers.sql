efa0cb6ea060c55c639b5cb8d308ffecfbe10c8b
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-07-28 10:30:32 +0000

    -

--- models/1_staging/vendor_portal/1_offers/stg_vp_offers.sql
+++ models/1_staging/vendor_portal/1_offers/stg_vp_offers.sql
@@ -16,18 +16,19 @@ select
     validityto as valid_to,
     maxdailyfulfillmentquantity as max_daily_fulfillment_quantity,
     availablequantity as available_quantity,
-    farmcatalogvariationsnapshot.active as variation_active_status,
+    -- farmcatalogvariationsnapshot.active as variation_active_status,
     farmcatalogvariationsnapshot.color as product_color,
     farmcatalogvariationsnapshot.minimumlengthofflowerstem as stem_length,
-    farmcatalogvariationsnapshot.countryoforigin as farm_origin,
+    farmcatalogvariationsnapshot.countryoforigin as vendor_region,
     farmcatalogvariationsnapshot.producttypename as product_type_name,
     farmcatalogvariationsnapshot. categoryname as product_category,
     farmsnapshot.name as Farm,
-    farmsnapshot.active as farm_active_status,
+    -- farmsnapshot.active as farm_active_status,
     farmsnapshot. currency as currency,
     vendorsnapshot. name as Vendor,
-    vendorsnapshot.active as vendor_active_status,
-
+    case when vendorsnapshot.active = True then "active" else "inactive" end as vendor_status,
+    case when farmcatalogvariationsnapshot.active = True then "active" else "inactive" end as variation_status,
+    case when farmsnapshot.active = True then "active" else "inactive" end as farm_status,
 
 
 from {{ source(var('erp_source'), 'vp_offers') }}
