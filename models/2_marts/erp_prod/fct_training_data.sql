ee763037a8338c200fdd55de346a0c8f8fb5d60a
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-08-12 11:42:22 +0000

    -

--- models/2_marts/erp_prod/fct_training_data.sql
+++ models/2_marts/erp_prod/fct_training_data.sql
@@ -4,6 +4,6 @@ left join {{ref ("int_line_items")}} li on li.line_item_id = t.line_item_id
 left join {{ref ("int_line_items")}} cli on cli.parent_line_item_id = li.line_item_id
 
 where cli.customer_type = 'retail' and t.warehouse = 'Dubai Warehouse' 
-and reseller_label = 'Express'
+and t.reseller_label = 'Express'
 and t.product in ('Rose Ever Red', 'Rose Athena', 'Chrysanthemum Spray Pina Colada', 'Gypsophila Xlence', 'Rose Madam Red')
 group by 1,2,3
