c16c36610811ee634da5a190202766bd2c9df537
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-09-07 06:25:29 +0000

    -

---
+++ models/1_staging/vendor_portal/0_orders/stg_vp_rejected_order_items.sql
@@ -0,0 +1,33 @@
+select 
+
+    -- PK
+    _id as order_item_id,
+
+
+    -- FK
+    orderid as order_id,
+    farmid as farm_id,
+    floranowfarmid as floranow_farm_id,
+    vendorid as vendor_id,
+    floranowvendorid as floranow_vendor_id,
+
+
+    -- Data 
+    ordernumber as order_number,
+    destination,
+    customerdebtornumber as debtor_number,
+    customername as Customer,
+    productname as Product,
+    currency,
+    departuredate as departure_date,
+    price as unit_price,
+    unitfobprice as unit_fob_price,
+    quantity,
+    status as order_item_status,
+    ordertype as order_type,
+    farmname as Farm,
+    vendorname as Vendor,
+    offernumber as offer_number,
+
+
+from {{ source(var('erp_source'), 'vp_rejected_order_items') }}
