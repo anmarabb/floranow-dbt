c9021be0733d92f8434134ec285474577a73481d
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-09-08 10:10:54 +0000

    -

---
+++ models/2_marts/vendor_portal/1_offers/fct_vp_calendar_offer.sql
@@ -0,0 +1,35 @@
+WITH calendar AS (
+  SELECT day
+  FROM UNNEST(GENERATE_DATE_ARRAY('2025-01-01', '2025-09-30', INTERVAL 1 DAY)) AS day
+)
+
+SELECT 
+  c.day,
+  o.offer_id,
+  o.offer_number,
+  o.product,
+  o.valid_from,
+  o.valid_to,
+  o.max_daily_fulfillment_quantity,
+  o.unit_price,
+  case when day = current_date() then available_quantity end as available_quantity,
+  product_color,
+  stem_length,
+  vendor_region,
+  product_type_name,
+  product_category,
+  Farm,
+  currency,
+  Vendor,
+  mainimage_url,
+  vendor_status,
+  variation_status,
+  farm_status,
+  offer_status,
+  number_of_days,
+  is_new_offer,
+  is_expiring_soon,
+
+FROM calendar c
+LEFT JOIN `dbt_prod_dwh.fct_vp_offers` o
+  ON c.day BETWEEN DATE(o.valid_from) AND DATE(o.valid_to)
