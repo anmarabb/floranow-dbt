369ff0288b0080376751e7e5f9a4ee8b37f8ea4d
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-10-20 09:46:05 +0000

    -

--- models/1_staging/erp_prod/9_forecast/stg_training_data.sql
+++ models/1_staging/erp_prod/9_forecast/stg_training_data.sql
@@ -1,2 +1,3 @@
-select *
+select * EXCEPT(product),
+       case when product in ('Ruscus Hypophyllum (large Leaf)', 'Ruscus') then 'Ruscus' else product end as product, 
 from {{ref ("fct_products")}}
