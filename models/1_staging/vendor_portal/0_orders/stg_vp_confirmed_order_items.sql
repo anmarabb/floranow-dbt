4d604f6ef8a6c4931f669becec1722d7f3b992f2
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-10-27 07:25:10 +0000

    -

--- models/1_staging/vendor_portal/0_orders/stg_vp_confirmed_order_items.sql
+++ models/1_staging/vendor_portal/0_orders/stg_vp_confirmed_order_items.sql
@@ -34,6 +34,7 @@ select
     date(confirmedat) as confirmed_at,
     cast(null as date) as rejected_at,
     cast(null as date) as cancelled_at,
+    null as reason,
 
 
 from {{ source(var('erp_source'), 'vp_confirmed_order_items') }}
