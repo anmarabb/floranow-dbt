0b6e979c72bc1daddd393ac349a2e10fb15f6bd9
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-12-09 13:53:34 +0000

    -

---
+++ models/2_marts/erp_prod/5_invoices/dim_stamped_invoices_tracking.sql
@@ -0,0 +1,16 @@
+select 
+       i.invoice_header_id,
+       i.pod_status,
+       i.invoice_number , 
+    --    i.is_stamped,
+       i.date_invoice_header_created_at ,
+       i.invoice_header_printed_at,
+       i.invoice_header_status,
+    --    i.proof_of_delivery_id
+       i.printed_by,
+       i.dispatched_by,
+    --    attached_by.name as attached_by,
+
+from {{ref('fct_invoices')}} as i
+-- left join attachment_references ar on CAST(i.id AS VARCHAR)= ar.record_id
+-- left join users attached_by on ar.user_id = attached_by.id
