3d85bb272243f6d526a2641df9e1f79e95ac6ef1
Nourhan Abu Zannad <nabuzannad@floranow.com>
2025-12-09 14:12:03 +0000

    -

--- models/2_marts/erp_prod/5_invoices/dim_stamped_invoices_tracking.sql
+++ models/2_marts/erp_prod/5_invoices/dim_stamped_invoices_tracking.sql
@@ -14,3 +14,4 @@ select
 from {{ref('fct_invoices')}} as i
 left join {{ref('stg_attachment_references')}} ar on CAST(i.invoice_header_id AS string)= ar.record_id
 left join {{ref('base_users')}} attached_by on ar.user_id = attached_by.id
+where ar.attachment_name = 'client_stamped_invoice'
