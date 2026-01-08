895fc747158ea16641b95c684d7e411bf1900975
NoorhanAbuZnad <165781871+NoorhanAbuZnad@users.noreply.github.com>
2026-01-08 09:04:02 +0300

    -

--- models/2_marts/erp_prod/5_invoices/fct_invoice_items.sql
+++ models/2_marts/erp_prod/5_invoices/fct_invoice_items.sql
@@ -84,6 +84,7 @@ select
     when debtor_number = '132008' then 'Intercompany Sales'
     when customer_type = 'reseller' then 'Intercompany Sales'
     when debtor_number = '130188' then 'Intercompany Sales'   
+    when debtor_number = '13200801' then 'Intercompany Sales'
     else 'Floranow Sales'
     end as inv_items_reprot_filter,
 
--- models/2_marts/erp_prod/5_invoices/fct_invoices.sql
+++ models/2_marts/erp_prod/5_invoices/fct_invoices.sql
@@ -10,6 +10,7 @@ case
     when debtor_number = '132008' then 'Intercompany Sales'
     when customer_type = 'reseller' then 'Intercompany Sales'
     when debtor_number = '130188' then 'Intercompany Sales'
+    when debtor_number = '13200801' then 'Intercompany Sales'
     else 'Floranow Sales'
     end as reprot_filter,
     
