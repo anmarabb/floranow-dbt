22c6d72e3e22230bed0763cf0744b41169c72f37
NoorhanAbuZnad <165781871+NoorhanAbuZnad@users.noreply.github.com>
2026-01-14 15:53:21 +0300

    -

--- models/2_marts/erp_prod/9_forecast/fct_daily_demand.sql
+++ models/2_marts/erp_prod/9_forecast/fct_daily_demand.sql
@@ -19,17 +19,18 @@ SELECT
     ac.date,
     ac.warehouse,
     ac.product,
-    COALESCE(orig.ordered, 0) AS ordered,
-    COALESCE(orig.sold, 0) AS sold,
-    COALESCE(orig.incidents, 0) AS incidents,
-    COALESCE(orig.warehoused, 0) AS warehoused,
-    COALESCE(orig.cumulative_remaining_quantity, 0) AS cumulative_remaining_quantity,
-    COALESCE(orig.age, ac.age, 0) AS age,
-    COALESCE(orig.product_category, ac.product_category, '') AS product_category
+    SUM(COALESCE(orig.ordered, 0)) AS ordered,
+    SUM(COALESCE(orig.sold, 0)) AS sold,
+    SUM(COALESCE(orig.incidents, 0)) AS incidents,
+    SUM(COALESCE(orig.warehoused, 0)) AS warehoused,
+    SUM(COALESCE(orig.cumulative_remaining_quantity, 0)) AS cumulative_remaining_quantity,
+    MAX(COALESCE(orig.age, ac.age, 0)) AS age,
+    MAX(COALESCE(orig.product_category, ac.product_category, '')) AS product_category
 FROM {{ ref('int_daily_demand_combinations') }} ac
 LEFT JOIN daily_demand_data orig
     ON ac.date = orig.date
     AND ac.warehouse = orig.warehouse
     AND ac.product = orig.product
+GROUP BY ac.date, ac.warehouse, ac.product
 ORDER BY ac.warehouse, ac.product, ac.date
 
