aedf51dd604d379a98fb752866cb126390cb0d95
NoorhanAbuZnad <165781871+NoorhanAbuZnad@users.noreply.github.com>
2026-02-05 15:42:26 +0300

    -

--- models/1_staging/erp_prod/9_forecast/stg_forecast_demand.sql
+++ models/1_staging/erp_prod/9_forecast/stg_forecast_demand.sql
@@ -1,2 +1,5 @@
-select *
+select * except(product, warehouse), 
+       product as Product,
+       case when date >= date_trunc(current_date(), month) then date_trunc(current_date(), month) else null end as period_start
+
 from {{ source(var('erp_source'), 'forecast_demand') }}
